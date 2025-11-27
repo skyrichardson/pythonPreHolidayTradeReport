bc68276ad6919ef27ba11ad56d949884f11f4e01
skyrichardson <86213662+skyrichardson@users.noreply.github.com>
2025-11-27 16:28:28 +1100

    Save ticker data to file

---
+++ get_ticker_data.py
@@ -0,0 +1,27 @@
+import os
+from datetime import datetime, timedelta
+import time
+import yfinance as yf
+
+with open('target_directory.txt', 'r') as f:
+    directory = f.read()
+
+# create working directory
+# os.mkdir(directory)
+
+date_obj = datetime.strptime(directory, '%Y-%m-%d')
+print(date_obj + timedelta(days=1))
+start_date_str = (date_obj - timedelta(days=15)).strftime('%Y-%m-%d')
+end_date_str = (date_obj + timedelta(days=1)).strftime('%Y-%m-%d')
+
+
+def get_ticker_history(stock_symbol):
+    ticker = yf.Ticker(stock_symbol)
+    data = ticker.history(start=start_date_str, end=end_date_str)
+    data.to_csv(f'{directory}/{stock_symbol.lower()}.csv', index=True)
+    time.sleep(0.5)
+    return data
+
+
+df = get_ticker_history('SPY')
+print(df.tail())
